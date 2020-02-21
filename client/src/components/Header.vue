2eb40574b0a9c316119b0cd0a9a9e4868b45ab8f
Thariq Audah <thariq.audah@gmail.com>
2020-02-21 16:49:06 +0700

    Update header navbar and navigation

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,30 +1,36 @@
 <template>
   <div id="app-navbar">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Stock Trading</a>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <router-link class="navbar-brand" :to="{name: 'home'}">Stock Trading</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
+      <!-- Main menu -->
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item ml-4">
             <router-link class="nav-link" :to="{name: 'home'}">Home</router-link>
           </li>
-          <li class="nav-item mr-5">
+          <li class="nav-item">
             <router-link class="nav-link" :to="{name: 'portfolio'}">Portfolio</router-link>
           </li>
-          <!-- <li class="nav-item dropdown">
+        </ul>
+
+        <!-- Dropdown menu -->
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link">End Day</a>
+          </li>
+          <li class="nav-item dropdown mr-5">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
+              Save & Load
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item" href="#">Save Data</a>
+              <a class="dropdown-item" href="#">Load Data</a>
             </div>
-          </li> -->
+          </li>
         </ul>
       </div>
     </nav>
